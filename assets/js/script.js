d4c2b2f540a0b2fa7806a0f72da84fb4eae3b3dd
Hunter Nelson <huntknelson@gmail.com>
2022-03-04 17:21:13 -0600

    created querySelectors for existing elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,15 @@
 
+//    Header 
+var timerEl = document.querySelector("#timer");
 
+// Question Elements
+var questionEl = document.querySelector("#main-heading");
+var textEl = document.querySelector("#q-text");
+var listEl = document.querySelector("quiz-list");
+var buttonEl = document.querySelector("#startbutton");
+startbutton
 
-
-
+//Answer Elements
 
 
 // GIVEN I am taking a code quiz
