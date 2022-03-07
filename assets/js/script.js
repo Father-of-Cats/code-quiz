a155900efc40b4185fbaede0bf512e2880219791
Hunter Nelson <huntknelson@gmail.com>
2022-03-06 18:56:58 -0600

    started hs function, not working currently

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-
+debugger
 //    Header 
 var timerEl = document.querySelector("#timer");
 
@@ -7,9 +7,28 @@ var questionEl = document.querySelector("#main-heading");
 var textEl = document.querySelector("#q-text");
 var listEl = document.querySelector("quiz-list");
 var buttonEl = document.querySelector("#startbutton");
-startbutton
+
+// High Score Elements
+var scoreEl = document.querySelector("#score")
+var nameInputEl = document.querySelector("#h-name")
+var scoreListEl = document.querySelector("#h-list")
+var scoreButtonEl = document.querySelector("#h-button")
 
 //Answer Elements
+
+
+// Highscore Functions
+
+scoreButtonEl.addEventListener("click", insertScore);
+
+function insertScore() {
+    event.preventDefault();
+    var highScoreEl = document.createElement("li");
+    highScoreEl.textContent = nameInputEl.value;
+    // highScoreEl.className = " "; add back after style
+    scoreListEl.
+};
+
 
 
 // GIVEN I am taking a code quiz
